a76d7cc1e6101174961e0c4e7156b8f8785de93d
devashishkumar <dev.ashishkumar0512@gmail.com>
2022-01-22 14:03:02 +0530

    solidity crowd funding app

---
+++ CrowdFunding.sol
@@ -0,0 +1,77 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity >=0.7.0 <0.9.0;
+
+contract CrowdFunding {
+
+    mapping(address => uint) public contributors;
+    address public manager;
+    uint public minimumContribution;
+    uint public deadline;
+    uint public target;
+    uint public raisedAmount;
+    uint public noOfContributors;
+
+    struct Request {
+        string description;
+        address payable recipient;
+        uint value;
+        bool completed;
+        uint noOfVoters;
+        mapping(address => bool) voters;
+    }
+    mapping(uint => Request) public requests;
+    uint public numRequests;
+
+    constructor(uint _target, uint _deadline) {
+        target = _target;
+        deadline = block.timestamp+_deadline;
+        minimumContribution = 100 wei;
+        manager = msg.sender;
+    }
+
+    function sendEth() public payable {
+        require(block.timestamp < deadline, "Deadline Passed");
+        require(msg.value >= minimumContribution, "Minimum contribution is not met");
+
+        if (contributors[msg.sender] == 0) {
+            noOfContributors++;
+        }
+        contributors[msg.sender] += msg.value;
+        raisedAmount += msg.value;
+    }
+
+    function getContractBalance() public view returns (uint) {
+        return address(this).balance;
+    }
+
+    function refund() public {
+        require(block.timestamp > deadline && raisedAmount < target, "You are not eligible for refund");
+        require(contributors[msg.sender] > 0);
+        address payable user = payable(msg.sender);
+        // user.transfer(100);
+        user.transfer(contributors[msg.sender]);
+        contributors[msg.sender] = 0;
+    }
+
+    modifier onlyManager() {
+        require(msg.sender == manager, "Only manager can use this method");_;
+    }
+
+    function createRequests(string memory _description, address payable _recipient, uint _value) public onlyManager {
+        Request storage newRequest = requests[numRequests];
+        numRequests++;
+        newRequest.description = _description;
+        newRequest.recipient = _recipient;
+        newRequest.value = _value;
+        newRequest.completed = false;
+        newRequest.noOfVoters = 0;
+    }
+
+    function voteRequest(uint _request) public {
+        require(contributors[msg.sender] > 0 , "You must be contributor");
+        Request storage currRequest = requests[_request];
+        require(currRequest.voters[msg.sender] == false, "You have already voted");
+        currRequest.noOfVoters++;
+    }
+}
